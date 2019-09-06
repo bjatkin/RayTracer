55e51b6e15f9e2cda3df85691d195f3890adb5f8
Brandon Atkinson <bj.atkinson92@gmail.com>
2019-09-06 12:19:02 -0600

    Make image more general and add PNG write metthod

---
+++ image.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"io"
+	"strconv"
+	"strings"
+)
+
+//Image represents a image file
+type Image struct {
+	width  int
+	height int
+	pixels []RGB
+}
+
+//NewImage returns a new image object
+func NewImage(width, height int) *Image {
+	px := make([]RGB, width*height)
+	return &Image{
+		width:  width,
+		height: height,
+		pixels: px,
+	}
+}
+
+//WritePPM writes out the Image as a vaild ppm file to an io.Writer
+func (p *Image) WritePPM(dest io.Writer) {
+	var pxs []string
+	for _, px := range p.pixels {
+		pxs = append(pxs, px.String())
+	}
+	dest.Write([]byte(
+		"P3\n" +
+			fmt.Sprintf("%d %d\n", p.width, p.height) +
+			"255\n" +
+			strings.Join(pxs, " "),
+	))
+}
+
+//WritePNG wirets out the image as a valid png file to the io.Writer
+func (p *Image) WritePNG(dest io.Writer) {
+	out := image.NewNRGBA(image.Rect(0, 0, p.width, p.height))
+
+	for y := 0; y < p.height; y++ {
+		for x := 0; x < p.width; x++ {
+			pxl := p.GetPixel(x, y)
+
+			out.Set(x, y, color.NRGBA{
+				R: pxl.R, //uint8((x + y) & 255),
+				G: pxl.G, //uint8((x + y) << 1 & 255),
+				B: pxl.B, //uint8((x + y) << 2 & 255),
+				A: 255,
+			})
+		}
+	}
+}
+
+//RGB is an rgb color with rgb values between 0 and 255
+type RGB struct {
+	R uint8
+	G uint8
+	B uint8
+}
+
+func (rgb RGB) String() string {
+	return strconv.FormatInt(int64(rgb.R), 10) + " " +
+		strconv.FormatInt(int64(rgb.G), 10) + " " +
+		strconv.FormatInt(int64(rgb.B), 10)
+}
+
+//SetPixel colors a pixel at the given x, y coord
+func (p *Image) SetPixel(x, y int, rgb RGB) {
+	p.pixels[y*p.width+x] = rgb
+}
+
+//GetPixel returns the rgb value of the pixel at x, y coord
+func (p *Image) GetPixel(x, y int) RGB {
+	return p.pixels[y*p.width+x]
+}
